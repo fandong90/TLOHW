ee269c0f8b8ba87e9bf3b8356452e91e331c0a17
fandong90 <871119239@qq.com>
2016-11-23 19:40:01 +0800

    33

---
+++ TLOHW/TLOHW/Controllers/HighChartController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TLOHW.Controllers
+{
+    public class HighChartController : Controller
+    {
+        // GET: HighChart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
